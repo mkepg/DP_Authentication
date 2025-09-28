0b679353af96352f1b54dd3690c813693f59ccba
Mikhael Edman Gomez <edman.gomez.business@gmail.com>
2025-09-29 00:22:40 +0800

    Modified DTO to be records

--- src/main/java/com/mm_mk/DP/Authentication/request/LoginRequest.java
+++ src/main/java/com/mm_mk/DP/Authentication/request/LoginRequest.java
@@ -1,12 +1,9 @@
 package com.mm_mk.DP.Authentication.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class LoginRequest {
-    @NotBlank
-    private String username;
-    @NotBlank
-    private String password;
-}
+public record LoginRequest(
+        @NotBlank String username,
+        @NotBlank String password
+) { }
+
--- src/main/java/com/mm_mk/DP/Authentication/response/AuthenticationResult.java
+++ src/main/java/com/mm_mk/DP/Authentication/response/AuthenticationResult.java
@@ -1,11 +1,6 @@
 package com.mm_mk.DP.Authentication.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class AuthenticationResult {
-    private String message;
-    private String token;
-}
+public record AuthenticationResult(
+        String message,
+        String token
+) { }
